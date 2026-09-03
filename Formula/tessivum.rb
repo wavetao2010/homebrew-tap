01091069801deea6310053d5626c6c193dac72a9
Tao Chan <chan@MacBook-Pro.local>
2026-09-03 10:52:59 +0800

    tessivum 0.1.0-alpha.21

--- Formula/tessivum.rb
+++ Formula/tessivum.rb
@@ -7,21 +7,21 @@ class Tessivum < Formula
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/wavetao2010/tessivum/releases/download/v0.1.0-alpha.20/tessivum-0.1.0-alpha.20-x86_64-apple-darwin.tar.gz"
-      sha256 "04ff7914f128c640fab0e8cc283003efea25406d3618c2b67a0569829a763a9c"
+      url "https://github.com/wavetao2010/tessivum/releases/download/v0.1.0-alpha.21/tessivum-0.1.0-alpha.21-x86_64-apple-darwin.tar.gz"
+      sha256 "9f13fbc5ccb5097e909594c4538062a0803acc1b6c54a9bf305b11db8824c1b5"
     elsif Hardware::CPU.arm?
-      url "https://github.com/wavetao2010/tessivum/releases/download/v0.1.0-alpha.20/tessivum-0.1.0-alpha.20-aarch64-apple-darwin.tar.gz"
-      sha256 "21a5980d05a8100a3d4e6b8448ac9c7124c4f8e30e33cda80e22911d0dc82308"
+      url "https://github.com/wavetao2010/tessivum/releases/download/v0.1.0-alpha.21/tessivum-0.1.0-alpha.21-aarch64-apple-darwin.tar.gz"
+      sha256 "e5efacf6bf35e0f5eef7238a383b6be8196475959e3cc5f3433244a37a7ef547"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/wavetao2010/tessivum/releases/download/v0.1.0-alpha.20/tessivum-0.1.0-alpha.20-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "995924951e7223c054553a49adb694a874d700c0e6499b75b05b7a8f13e9f5f1"
+      url "https://github.com/wavetao2010/tessivum/releases/download/v0.1.0-alpha.21/tessivum-0.1.0-alpha.21-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "29a91df27b615765ebe24b444cd0e77f07046f9dd99d296cc65830afde78fbd1"
     elsif Hardware::CPU.arm?
-      url "https://github.com/wavetao2010/tessivum/releases/download/v0.1.0-alpha.20/tessivum-0.1.0-alpha.20-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "c75ebf32775f0c7523b86d7c29ef56ce219aa5cd99b6499ce588b4fcd147a87b"
+      url "https://github.com/wavetao2010/tessivum/releases/download/v0.1.0-alpha.21/tessivum-0.1.0-alpha.21-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "b4d5720b79dc4e5e89032061d4c2624456597355780ab811cd4bb9e9279ec649"
     end
   end
 
